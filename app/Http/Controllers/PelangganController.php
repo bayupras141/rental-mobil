1c8f6ec0db983a969173d8e0dbf2b1e31b9f0346
Arfiyan <arfieyan1903@gmail.com>
2021-12-01 11:37:47 +0700

    customer insert

---
+++ app/Http/Controllers/PelangganController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Pelanggan;
+use Illuminate\Http\Request;
+
+use DataTables;
+
+class PelangganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Pelanggan::all();
+            return Datatables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+
+                        
+                        $btn =  '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="edit" class="text-secondary editData">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 "><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
+                        </a> | <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="text-secondary deleteData">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash "><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                        </a>'; 
+
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+
+
+        return view('pelanggan.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //end
+        $this->validate($request, [
+              'nama' =>                'required',
+                'username' =>            'required',
+                'nik' =>                 'required',
+                'email' =>               'required',
+                'alamat' =>              'required',
+                'no_hp' =>               'required',
+                'jenis_kelamin' =>       'required'
+        ]);
+
+        $pelanggan = Pelanggan::updateOrCreate(
+            ['id' => $request->data_id],
+            [
+                'nama' => $request->nama,
+                'username' => $request->username,
+                'nik' => $request->nik,
+                'email' => $request->email,
+                'password' => $request->username,
+                'alamat' => $request-> alamat,
+                'no_hp' => $request-> no_hp,
+                'jenis_kelamin' => $request-> jenis_kelamin
+            ]
+        );
+
+        return response()->json([
+            'status' => 'sukses',
+            'message'=>'Mobil berhasil Ditambahkan'
+        ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelanggan $pelanggan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelanggan $pelanggan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pelanggan $pelanggan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pelanggan $pelanggan)
+    {
+        //
+    }
+}
