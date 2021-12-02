e90f25264b52a1966c6cfa4e34ecfe2f8d54c5f0
bayupras141 <aebayu789@gmail.com>
2021-12-02 07:50:40 +0700

    perubahan

--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -125,8 +125,8 @@
 
     
 
-<script>
-     $(document).ready(function($){
+    <script>
+        $(document).ready(function($){
             $.ajaxSetup({
                 headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -149,9 +149,8 @@
                 ]
             });
 
-    // button create new data
-             // show modal
-             $('#createNewData').click(function () { 
+            // button create new data
+            $('#createNewData').click(function () { 
                 $('#saveBtn').val("create-pelanggan");
                 $('#data_id').val('');
                 $('#dataForm').trigger("reset");
@@ -160,10 +159,10 @@
                 $("#errors-validate").hide();
                 $('#saveBtn').prop('disabled', false);
             });
-
-    // end 
-      // store process
-      $('#saveBtn').click(function (e) {
+            // end 
+            
+            // store process
+            $('#saveBtn').click(function (e) {
                 e.preventDefault();
                 $(this).html('Sending..');
 
@@ -190,71 +189,70 @@
               });
             });
             // end store process
-               // edit data
-    $('body').on('click', '.editData', function () {
-var data_id = $(this).data('id');
-$.get("{{ route('pelanggan.index') }}" +'/' + data_id +'/edit', function (data) {
-    $('#saveBtn').html("Update");  
-    $('#modalHeading').html("Edit Data");
-    $('#modalBox').modal('show');
-    $("#errors-validate").hide();
-     $('#saveBtn').prop('disabled', false);
-    // get data respone
-    $('#data_id').val(data.id);
-    $('#nama').val(data.nama);
-          $('#username').val(data.username);
-          $('#nik').val(data.nik);
-          $('#email').val(data.email);
-          $('#alamat').val(data.alamat);
-          $('#no_hp').val(data.no_hp);
-          if(data.jenis_kelamin == 'Laki-Laki'){
-                $("#validationRadiojq1").prop("checked", true);
-            }else{
-                $("#validationRadiojq2").prop("checked", true);
-            }
-})  });
-// end
-            // delete
-    $('body').on('click', '.deleteData', function () {
 
-        var data_id = $(this).data("id");
-        Swal.fire({
-        title: "Apa kamu yakin?",
-        text: "Menghapus data ini!",
-        icon: "warning",
-        buttons: [
-        'Tidak',
-        'Iya'
-        ],
-        dangerMode: true,
-        }).then(function(isConfirm) {
-        if (isConfirm) {
-        Swal.fire({
-        title: 'Selamat!',
-        text: 'Data berhasil di hapus!',
-        icon: 'success'
-        }).then(function() {
-            $.ajax({
-                type: "DELETE",
-                url: "{{ route('pelanggan.store') }}"+'/'+data_id,
-                success: function (data) {
-                    table.draw();
-                },
-                error: function (data) {
-                    console.log('Error:', data);
-                }
+            // edit data
+            $('body').on('click', '.editData', function () {
+                var data_id = $(this).data('id');
+                $.get("{{ route('pelanggan.index') }}" +'/' + data_id +'/edit', function (data) {
+                    $('#saveBtn').html("Update");  
+                    $('#modalHeading').html("Edit Data");
+                    $('#modalBox').modal('show');
+                    $("#errors-validate").hide();
+                     $('#saveBtn').prop('disabled', false);
+                    // get data respone
+                    $('#data_id').val(data.id);
+                    $('#nama').val(data.nama);
+                          $('#username').val(data.username);
+                          $('#nik').val(data.nik);
+                          $('#email').val(data.email);
+                          $('#alamat').val(data.alamat);
+                          $('#no_hp').val(data.no_hp);
+                          if(data.jenis_kelamin == 'Laki-Laki'){
+                                $("#validationRadiojq1").prop("checked", true);
+                            }else{
+                                $("#validationRadiojq2").prop("checked", true);
+                            }
+                })  
             });
+            // end
+
+            // delete
+            $('body').on('click', '.deleteData', function () {
+                var data_id = $(this).data("id");
+                Swal.fire({
+                title: "Apa kamu yakin?",
+                text: "Menghapus data ini!",
+                icon: "warning",
+                buttons: [
+                'Tidak',
+                'Iya'
+                ],
+                dangerMode: true,
+                }).then(function(isConfirm) {
+                if (isConfirm) {
+                Swal.fire({
+                title: 'Selamat!',
+                text: 'Data berhasil di hapus!',
+                icon: 'success'
+                }).then(function() {
+                    $.ajax({
+                        type: "DELETE",
+                        url: "{{ route('pelanggan.store') }}"+'/'+data_id,
+                        success: function (data) {
+                            table.draw();
+                        },
+                        error: function (data) {
+                            console.log('Error:', data);
+                        }
+                    });
+                });
+                } else {
+                    Swal.fire("Cencel", "Data tidak jadi dihapus :)", "error");
+                }
+                })
+            });        
+            // end
+            
         });
-        } else {
-        Swal.fire("Cencel", "Data tidak jadi dihapus :)", "error");
-        }
-        })
-
-
-        });
-
-    // end
-        });
-
-</script>
+    </script>
 @endpush
